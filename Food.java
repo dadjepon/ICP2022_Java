60c29b85a43318188b2027ff8c3ecb3a7e734800
David Ebo Adjepon-Yamoah <105603787+dadjepon@users.noreply.github.com>
2022-09-16 10:36:44 +0000

    Add files via upload

--- Food.java
+++ Food.java
@@ -3,10 +3,15 @@
  */
 package ashesi.edu.gh.ICP313;
 import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.FileReader;
 import java.lang.ArrayIndexOutOfBoundsException;
+import java.util.Scanner;
 /**
  * @author David Ebo Adjepon-Yamoah, Dr. Sowah
  * @author Banahene-Owusu Osei
@@ -59,7 +64,7 @@ public double getfoodPrice( ) {
 	return this.price;
 }
 /*
- * returns teh name of the chef
+ * returns the name of the chef
  */
 public String getChefName() {
 	return this.chefName;
@@ -131,46 +136,135 @@ public int hashCode() {
 	}
 
 	/**
+	 * BufferedReader Example 4
+	 * @throws IOException
+	 */
+	public static void readingWithBuffering() throws IOException
+	{
+		FileReader in1 =null;
+		BufferedReader br= null;
+		try
+		{
+			in1=new FileReader("C:\\Users\\dadjepon\\eclipse-workspace\\ashesi.edu.gh.ICP313\\src\\ashesi\\edu\\gh\\ICP313\\Data.txt");
+			br= new BufferedReader(in1);
+			String x;
+		while ((x=br.readLine()) != null)
+		{
+			System.out.println(x);
+		}
+		}finally
+		{
+			if (br !=null)
+				br.close();
+		}
+	}
+
+
+	/**
 	 * @param args
 	 * @return void
+	 * @throws IOException 
 	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stu
+	public static void main(String[] args) throws IOException {
+		//EXAMPLE 4: BufferedReader 
+		readingWithBuffering();
 		
-		Food food = new Food("Banku", "Charles", 100.0, 
-				"Ghana", "Vegan", 15.50);
+		//EXAMPLE 1: INCOMPLETE
+		Scanner scan = new Scanner(System.in);
+
+		System.out.println("Enter Food Name: ");
+		String foodName = scan.nextLine();
+		
+		System.out.println("Enter Chef Name: ");
+		String chefName = scan.nextLine();
+		
+		System.out.println("Enter amount of calories: ");
+		double calories = scan.nextDouble();
+		
+		//Interesting
+		scan.nextLine();
+		
+		System.out.println("Enter origin of food: ");
+		String cName = scan.nextLine();
+		
+		System.out.println("Enter dietary requirements: ");
+		String dietReq = scan.nextLine();
+		
+		System.out.println("Enter food price: ");
+		double foodPrice = scan.nextDouble();
+		
+		Food food = new Food(foodName, chefName, calories, 
+				cName, dietReq, foodPrice);
 		
 		System.out.println("Food Name: "+food.getFoodName());
 		System.out.println(food.toString());
 		food.setFoodName("");
 		System.out.println(food.getFoodName());
 		
-		try {
-			File file = new File("/home/icp/file.txt");
-			FileReader fileReader = new FileReader(file);
-		}
-		//The file isnt there
-		catch(FileNotFoundException fnfe) {
-			System.out.println("Exception caught: "+fnfe.getMessage());
-		}
-		//Catch all input and output exceptions
-		catch(IOException ioe) {
-			ioe.printStackTrace();
-			System.out.println("Exception caught!");	
+		/*
+		 * try { File file = new File(
+		 * "C:\\Users\\dadjepon\\eclipse-workspace\\ashesi.edu.gh.ICP313\\src\\ashesi\\edu\\gh\\ICP313\\file.txt"
+		 * );
+		 * 
+		 * @SuppressWarnings({ "unused", "resource" }) FileReader fileReader = new
+		 * FileReader(file); } //The file isn't there catch(FileNotFoundException fnfe)
+		 * { System.out.println("Exception caught: "+fnfe.getMessage()); } //Catch all
+		 * input and output exceptions catch(@SuppressWarnings("hiding") IOException
+		 * ioe) { ioe.printStackTrace(); System.out.println("Exception caught!");
+		 * 
+		 * }
+		 * 
+		 * try { int array[] = {1, 2, 3, 4, 5}; System.out.println(array[2]); } // catch
+		 * any indices that are outside the range of the array catch
+		 * (ArrayIndexOutOfBoundsException ae) { ae.printStackTrace();
+		 * System.out.println("Exception caught!" + ae.getMessage()); }
+		 */
 		
-		}
-		
-		try {	
-			int array[] = {1, 2, 3, 4, 5};
-			System.out.println(array[7]);
-		}
-		// catch any indices that are outside the range of the array
-		catch (ArrayIndexOutOfBoundsException ae) {
-			ae.printStackTrace();
-			System.out.println("Exception caught!" + ae.getMessage());
-		}
-
+		//EXAMPLE 2:
+		 String data = "This is a text inside the file.";
 		 
+		 try { 
+			PrintWriter output = new PrintWriter(
+					"C:\\Users\\dadjepon\\eclipse-workspace\\ashesi.edu.gh.ICP313\\src\\ashesi\\edu\\gh\\ICP313\\output.txt");
+		 
+		 	output.print(data); 
+		 	output.close(); 
+		 	} 
+		 catch(Exception e) { e.getStackTrace();}
+		 
+
+		/**
+		 * TASK: Demo of the Scanner, PrintWriter, 
+		 * FileInputStream, FileOutputStream Classes
+		 */
+		Scanner inputStream = null;
+		PrintWriter outputStream = null;
+		
+		try {
+			inputStream = new Scanner(
+					new FileInputStream("C:\\Users\\dadjepon\\eclipse-workspace\\ashesi.edu.gh.ICP313\\src\\ashesi\\edu\\gh\\ICP313\\original.txt"));
+			outputStream = new PrintWriter(
+					new FileOutputStream("C:\\Users\\dadjepon\\eclipse-workspace\\ashesi.edu.gh.ICP313\\src\\ashesi\\edu\\gh\\ICP313\\food.txt"));
+		}
+		catch(FileNotFoundException fnfe) {
+			System.out.println("Problem opening file:"
+					+fnfe.getMessage());
+		}
+		finally {
+			//Gracefully closing the program by 
+			//killing the current JVM
+			System.exit(0);}
+		
+		String line = null;
+		int count = 0;
+		
+		while(inputStream.hasNextLine()) {
+			line = inputStream.nextLine();
+			count++;
+			outputStream.println(count + " " + line);
+		}
+		inputStream.close();
+		outputStream.close();
 	}
 
 }
